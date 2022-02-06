0519a506459be1fce9400f1d8659e3c1ee1a8d3f
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-02-06 00:28:57 -0500

    Remove default submit behavior on the submit form tag

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,11 @@ import React from "react";
 
 class SearchBar extends React.Component {
   state = {
-    term: ''
+    inputValue: ''
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
   }
 
   //onChange att is a callback function so the reference to the method needs to be without parentesis
@@ -10,13 +14,13 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search: </label>
             <input
               type='text'
-              value={this.state.term}
-              onChange={(e) => this.setState({ term: e.target.value })} />
+              value={this.state.inputValue}
+              onChange={e => this.setState({ inputValue: e.target.value })} />
           </div>
         </form>
       </div>
